c1ca487e9b250b7af89addfbb0c96dc727c5775d
dormedina111 <110923343+dormedina111@users.noreply.github.com>
2024-05-24 00:55:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
 const supabase = require('./supabaseClient'); 
-
 const port = process.env.PORT;
 app.use(cors()); 
 app.use(express.json());
-
 app.get('/api/code-blocks', async (req, res) => {
     const { data, error } = await supabase
         .from('code_blocks')
@@ -18,21 +16,16 @@ app.get('/api/code-blocks', async (req, res) => {
     }
     res.json(data);
 });
-
-
 const server = http.createServer(app);
-
 // Initialize socket.io with the created HTTP server
 const io = new Server(server, {
-    cors: {
+    cors: { 
         origin: '*', 
         methods: ['GET', 'POST'], 
     }
 });
-
 // Object to store room data
 let roomData = {};
-
 // Event handler for socket connections
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
@@ -41,48 +34,29 @@ io.on('connection', (socket) => {
     socket.on('joinRoom', (roomId) => {
         socket.join(roomId);
         console.log(`User ${socket.id} joined room ${roomId}`);
-
         // Initialize room data if it doesn't exist
         if (!roomData[roomId]) {
             roomData[roomId] = {
                 users: new Set()
             };
         }
-
         // Add user to the room and determine their role (mentor or student)
         roomData[roomId].users.add(socket.id);
-
         const isMentor = roomData[roomId].users.size === 1;
         const role = isMentor ? 'mentor' : 'student';
-
         // Emit roleAssigned event to the client with the assigned role
         socket.emit('roleAssigned', role);
         // Emit updateCode event to the client with the code in the room
         socket.emit('updateCode', roomData[roomId].code);
-
         // Event handler for updating code in the room
         socket.on('updateCode', (code) => {
             roomData[roomId].code = code;
             socket.to(roomId).emit('updateCode', code);
         });
-
          // Event handler for code solved
          socket.on('codeSolved', (solved) => {
             socket.to(roomId).emit('codeSolved', solved);
         });
-
-        // Event handler for user leave room
-        socket.on('leaveRoom', (id) => {
-            console.log('A user left room:', socket.id);
-            if (roomData[id]) {
-                roomData[id].users.delete(socket.id);
-                // Delete the room if it becomes empty
-                if (roomData[id].users.size === 0) {
-                    delete roomData[id];
-                }
-            }
-        });
-
         // Event handler for user disconnection
         socket.on('disconnect', () => {
             console.log('A user disconnected:', socket.id);
@@ -96,8 +70,6 @@ io.on('connection', (socket) => {
         });
     });
 });
-
-
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
